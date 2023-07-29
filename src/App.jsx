3c07b62b55a425988a426e8c95c9145d2fcc13ae
akashchatterjee3502 <akashchatterjee3502@gmail.com>
2023-07-29 11:38:11 +0530

    My first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-
+import Header from "./components/Header/header";
+import Hero from "./components/Hero/hero";
+import './App.css';
+import Companies from "./components/Companies/Companies";
+import Residencies from "./components/Residencies/Residencies";
+import Value from "./components/Value/Value";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+    <div className="App">
+      <div>
+        <div className="white-gradient"/>
+        <Header/>
+        <Hero/>
+      <Companies/>
+      </div>
+      <Residencies/>
+      <Value/>
+      <Contact/>
+      <Footer/>
+    </div>
   );
 }
 
